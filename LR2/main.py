4837b1835003caa7fa6ad8e6ffe6e4d20179d505
Svetlana <s.chernyshkova97@gmail.com>
2018-04-02 16:33:31 +0400

    2lr, final version

--- LR2/main.py
+++ LR2/main.py
@@ -1,7 +1,6 @@
 import math
 import cmath
 import numpy
-import scipy.fftpack
 import matplotlib.pyplot as pl
 
 
@@ -13,8 +12,8 @@ def gaussian_beam(x):
     return numpy.exp(-(x * x))
 
 
-def f_Fourie(x):
-    return -4*gaussian_beam(x)*x*(2*x*x-3)/numpy.power(2*math.pi*1j, 3)
+def f_fourier(x):
+    return -math.pi * math.sqrt(math.pi) * gaussian_beam(math.pi * x) * x * (2 * math.pi * math.pi * x * x - 3) / (2 * 1j)
 
 
 def amplitude(func):
@@ -38,7 +37,7 @@ def swap_half(a):
 
 
 def append_zeros(func, number_of_zeros):
-    f_with_zeros = numpy.zeros(2*number_of_zeros+len(func))
+    f_with_zeros = numpy.zeros(2 * number_of_zeros + len(func))
     for i in range(len(func)):
         f_with_zeros[number_of_zeros + i] = func[i]
     return f_with_zeros
@@ -53,19 +52,19 @@ def extract_function(func, N, number_of_zeros):
 
 def finite_fourier_transform(func, N, M, hx, number_of_zeros):
     f1 = append_zeros(func, int(number_of_zeros))
-    swap_half(f1)
-    F = hx * scipy.fft(f1, M)
-    swap_half(F)
+    f1 = numpy.fft.fftshift(f1)
+    F = hx * numpy.fft.fft(f1, M)
+    F = numpy.fft.ifftshift(F)
     F = extract_function(F, N, int(number_of_zeros))
     return F
 
 
 def my_ftt(func, a, b, N, hx, hu):
-    sumf = [0j]*M
+    sumf = [0j] * M
     for i in range(M):
         u = -b + i * hu
         for k in range(N):
-            r = cmath.exp(-2 * math.pi * u * (-a + k * hx)*1j)
+            r = cmath.exp(-2 * math.pi * u * (-a + k * hx) * 1j)
             f_mas = func(-a + k * hx)
             sumf[i] += f_mas * r
         sumf[i] *= hx
@@ -84,32 +83,35 @@ if __name__ == '__main__':
     x = numpy.linspace(-a, a, N)
     x1 = numpy.linspace(-b, b, N)
     x2 = numpy.linspace(-b, b, M)
+    x3 = numpy.linspace(-a, a, M)
 
-    gaus_beam = gaussian_beam(x)
+    # gaus_beam = gaussian_beam(x)
+    # gaus_beam_f = finite_fourier_transform(gaus_beam, N, M, hx, number_of_zeros)
+    # gaus_beam_f2 = my_ftt(gaussian_beam, a, b, N, hx, hu)
+
     my_beam = f(x)
-
-    ones = numpy.ones(N)
-
-    gaus_beam_f = finite_fourier_transform(gaus_beam, N, M, hx, number_of_zeros)
-    gaus_beam_f2 = my_ftt(gaussian_beam, a, b, N, hx, hu)
     my_beam_f = finite_fourier_transform(my_beam, N, M, hx, number_of_zeros)
     my_beam_f2 = my_ftt(f, a, b, N, hx, hu)
 
     """
-    pl.subplot(121)
-    pl.title('Ампитуда')
+    pl.subplot(131)
+    pl.plot(x, gaussian_beam(x))
+    pl.title("Пучок Гаусса")
+    pl.grid()
+
+    pl.subplot(132)
+    pl.title('Амплитуда')
     pl.plot(x, amplitude(gaus_beam), label='Исходный')
     pl.plot(x1, amplitude(gaus_beam_f), label='Фурье встр.')
-    pl.plot(x2, amplitude(gaus_beam_f2), label='Фурье моё')
-
+    pl.plot(x2, amplitude(gaus_beam_f2), label='Фурье числ.')
     pl.grid()
     pl.legend()
 
-    pl.subplot(122)
+    pl.subplot(133)
     pl.title('Фаза')
     pl.plot(x, phase(gaus_beam), label='Исходный')
     pl.plot(x1, phase(gaus_beam_f), label='Фурье встр.')
-    pl.plot(x2, phase(gaus_beam_f2), label='Фурье моё')
+    pl.plot(x2, phase(gaus_beam_f2), label='Фурье числ.')
     pl.gca().set_ylim(-math.pi, math.pi)
     pl.grid()
     pl.legend()
@@ -124,8 +126,8 @@ if __name__ == '__main__':
     pl.title('Амплитуда')
     pl.plot(x, amplitude(my_beam), label='Исходный')
     pl.plot(x1, amplitude(my_beam_f), label='Фурье встр.')
-    pl.plot(x2, amplitude(my_beam_f2), label='Моё')
-    pl.plot(x, amplitude(f_Fourie(x)*50), label='Аналитика')
+    # pl.plot(x2, amplitude(my_beam_f2), label='Моё')
+    pl.plot(x, amplitude(f_fourier(x)), label='Аналитика', color='c')
     pl.grid()
     pl.legend()
 
@@ -133,9 +135,9 @@ if __name__ == '__main__':
     pl.title('Фаза')
     pl.plot(x, phase(my_beam), label='Исходный')
     pl.plot(x1, phase(my_beam_f), label='Фурье встр.')
-    pl.plot(x2, phase(my_beam_f2), label='Моё')
-    pl.plot(x, phase(f_Fourie(x)*50), label='Аналитика')
-    pl.gca().set_ylim(-math.pi, math.pi)
+    # pl.plot(x2, phase(my_beam_f2), label='Моё')
+    pl.plot(x, phase(f_fourier(x)), label='Аналитика', color='c')
+    # pl.gca().set_ylim(-math.pi, math.pi)
     pl.grid()
     pl.legend()
 
